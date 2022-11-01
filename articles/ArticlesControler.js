a55330dfc8cae80b8b00b419c352346ca21f6006
Matheus <matheus@8n1.com.br>
2022-11-01 13:20:45 -0300

    implement article editor

--- articles/ArticlesControler.js
+++ articles/ArticlesControler.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const Category = require('../categories/Category');
 const router = express.Router();
 
-const app = express()
-const port = 3000
-
-router.get('/articles', (req, res) => {
-    res.send('Hello World! Articles');
-});
+// router.get('/articles', (req, res) => {
+//     // res.send("Route to create a new article");
+// });
 
 router.get("/admin/articles/new", (req,res) => {
-    res.send("Route to create a new article");
-})
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    });
+});
+
+// router.get("/admin/articles")
 
 module.exports = router;
